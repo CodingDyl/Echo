a04d7f2c64ffa41b172f920cb16a1daf4e481da7
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-05-15 11:20:07 +0200

    home page completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {styles} from '../styles'
 import { motion } from 'framer-motion'
+import TrailerCanvas from './canvas/Trailer'
 
 const  Home = () => {
   return (
@@ -13,6 +14,14 @@ const  Home = () => {
           </p>
         </div>
         </div>
+        <TrailerCanvas />
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white-100 flex justify-center items-start p-2">
+            <motion.div animate={{y: [0, 24, 0]}} transition={{duration: 1.5, repeat: Infinity, repeatType: 'loop' }} className="w-3 h-3 rounded-full bg-primary mb-1"/>
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
